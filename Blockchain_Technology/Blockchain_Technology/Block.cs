4ed5c958fdc1517f17a4c8b2ec6de6a98f21713c
Mert KAMALI <74986145+mertkamalii@users.noreply.github.com>
2022-05-12 16:58:48 +0300

    Add files via upload

---
+++ Blockchain_Technology/Blockchain_Technology/Block.cs
@@ -0,0 +1,50 @@
+﻿// PROGRAMLAMA DİLLERİNİN PRENSİPLERİ
+// B201200037 MERT KAMALI
+
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace Blockchain_Technology
+{
+    public class Block
+    {
+        public IList<Transaction> Transactions { get; set; }
+        public int ID { get; set; }
+        public int Nonce { get; set; } = 0;
+        public DateTime TimeStamp { get; set; }
+        public string Hash { get; set; }
+        public string PreviousHash { get; set; }        
+       
+        //public string Data { get; set; }
+        
+        public Block(DateTime timeStamp, string previousHash, IList<Transaction> transactions)
+        {
+            ID = 0;
+            TimeStamp = timeStamp;
+            PreviousHash = previousHash;
+            //Data = data;
+            Transactions = transactions;
+            //Hash = CalculateHash();
+        }
+        public string CalculateHash()
+        {
+            SHA256 sha256 = SHA256.Create();
+            byte[] inbytes = Encoding.ASCII.GetBytes($"{TimeStamp}-{PreviousHash ?? ""}-{JsonConvert.SerializeObject(Transactions)}-{Nonce}");
+            byte[] outbytes = sha256.ComputeHash(inbytes);
+            return Convert.ToBase64String(outbytes);
+        }
+        public void Mine(int difficulty)
+        {
+            var leadingZeros = new string('0', difficulty);
+            while (this.Hash == null || this.Hash.Substring(0, difficulty) != leadingZeros)
+            {
+                this.Nonce++;
+                this.Hash = this.CalculateHash();
+            }
+        }
+    }
+}
---
+++ Blockchain_Technology/Blockchain_Technology/Blockchain.cs
@@ -0,0 +1,80 @@
+﻿// PROGRAMLAMA DİLLERİNİN PRENSİPLERİ
+// B201200037 MERT KAMALI
+
+using System;
+using System.Collections.Generic;
+
+
+namespace Blockchain_Technology
+{
+    public class Blockchain
+    {
+        IList<Transaction> PendingTransactions = new List<Transaction>();
+        public IList<Block> Chain { get; set; }
+        public int difficulty { get; set; } = 2;
+        public int Reward { get; set; } = 2;
+
+        public Blockchain()
+        {
+            InitializeChain();
+            AddGenesisBlock();
+        }
+        public void InitializeChain()
+        {
+            Chain = new List<Block>();
+        }
+        public Block CreateGenesisBlock()
+        {
+            Block block = new Block(DateTime.Now, null, PendingTransactions);
+            block.Mine(difficulty);
+            PendingTransactions = new List<Transaction>();
+
+            return block;
+        }
+        public void AddGenesisBlock()
+        {
+            Chain.Add(CreateGenesisBlock());
+        }
+        public Block GetLatestBlock()
+        {
+            return Chain[Chain.Count - 1];
+        }
+        public void AddBlock(Block block)
+        {
+            Block latestblock = GetLatestBlock();
+            block.ID = latestblock.ID + 1;
+            block.PreviousHash = latestblock.Hash;
+            block.Hash = block.CalculateHash();
+            block.Mine(this.difficulty);
+            Chain.Add(block);
+        }
+        public void CreateTransaction( Transaction transaction)
+        {
+            PendingTransactions.Add(transaction);
+        }
+        public void ProcessPendingTransactions(string minerAddress)
+        {
+            Block block = new Block(DateTime.Now, GetLatestBlock().Hash, PendingTransactions);
+            AddBlock(block);
+            PendingTransactions = new List<Transaction>();
+            CreateTransaction(new Transaction(null, minerAddress, Reward));
+        }
+        public bool IsValid()
+        {
+            for (int i = 1; i < Chain.Count; i++)
+            {
+                Block currentBlock = Chain[i];
+                Block previousBlock = Chain[i - 1];
+                if (currentBlock.Hash!=currentBlock.CalculateHash())
+                {
+                    return false;
+                }
+                if (currentBlock.PreviousHash!=previousBlock.Hash)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
